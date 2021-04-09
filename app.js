a5c044e581c85e391bf3a76475c7288edc7f3464
laurenperez <laurenashleymarks@gmail.com>
2021-04-09 12:27:05 -0700

    update and clean up

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 
-$( function() {
-    $( ".draggable" ).draggable();
-  } );
+$(function() {
+    $(".draggable").draggable();
+});
 
-$( function() {
-   $( "#button" ).on( "click", function() {
-     $( ".myDiv" ).toggleClass( "myClass", 1000 );
-   });
- } );
+$(function() {
+  $("#button").on("click", function() {
+    $(".redDiv").toggleClass("yellowDiv", 1000);
+  });
+});
 
-$( function() {
-    $( "#datepicker" ).datepicker();
-  } );
+$(function() {
+  $("#datepicker").datepicker();
+});
 
-  $( function() {
-      $( "#accordion" ).accordion();
-    } );
+$(function() {
+  $("#accordion").accordion();
+});
